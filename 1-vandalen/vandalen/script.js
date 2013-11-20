2b98ba38b4176f63cf3642bff14179e8f006795f
OpenShift guest <th222fa@student.lnu.se>
2013-11-20 16:54:40 -0500

    vandalen är nu klar

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -5,17 +5,18 @@ var makePerson = function(persArr){
 var nameArr = [];
 var ageArr = [];
 var totalAge = 0;
-var averageAge;
+
 var i = 0;
 
 for(i; i < persArr.length; i++){
     nameArr.push(persArr[i].name);
     ageArr.push(persArr[i].age);
     totalAge += persArr[i].age;
-    console.log([i]);
 }
 
-nameArr.sort();
+nameArr.sort(function(a, b){
+return a.localeCompare(b);
+});
 ageArr.sort();
 
 var obj = {
@@ -31,4 +32,5 @@ return obj;
 
 var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}];
 
-var obj = makePerson(data);
+var result = makePerson(data);
+console.log(result);
