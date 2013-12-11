e4decd3ea58ce05fdfeac79318518a5f73028020
OpenShift guest <th222fa@student.lnu.se>
2013-12-11 10:35:35 -0500

    kan äntligen vända på bilderna! Körde fast lite på det där

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -3,11 +3,13 @@
 var Memory = {
     
     boards: [],
+    rows: 4,
+    cols: 4,
     
-    init: function(rows, cols){
+    init: function(){
        // alert("test");
         
-        Memory.boards = RandomGenerator.getPictureArray(4, 4);
+        Memory.boards = RandomGenerator.getPictureArray(Memory.rows, Memory.cols);
         
         console.log(Memory.boards);
         
@@ -18,41 +20,42 @@ var Memory = {
     
     RunPicture: function(){
         
+        var memoryPic = -1;
         var main = document.getElementById("main");
         var table = document.createElement("table");
         table.className = "table";
         main.appendChild(table);
         
-        for (var rows = 0; rows < 4 ; rows++) {
+        for (var r = 0; r < Memory.rows; r++) {
             
           var tr = document.createElement("tr");
           table.appendChild(tr);
         
-         for (var cols = 0; cols < 4; cols++){
+         for (var c = 0; c < Memory.cols; c++){
              
           var img = document.createElement("img");
           img.setAttribute( "src", "pics/0.png");
                 
           var a = document.createElement("a");
-          a.id="backSide"+ cols;
           var td = document.createElement("td");
           tr.appendChild(td);
           a.appendChild(img);
           a.href = "#";
           td.appendChild(a);
+          memoryPic+=1;
           
-          Memory.turnPicture(a);
+          Memory.turnPicture(a, memoryPic);
          }
          
         }
         
     },
     
-     turnPicture: function(a){
+     turnPicture: function(a, memoryPic){
          
          a.onclick = function(){
              
-             
+             a.querySelector("img").setAttribute("src", "pics/" + Memory.boards[memoryPic] + ".png");
              
          };
         
